a2ba79b266de53adf380e832210554cb77e2b3fa
Lucas Brémond <lucas.bremond@gmail.com>
2019-01-08 12:41:41 -0800

    [feature] Improve Python packaging

--- tools/development/helpers/clean.sh
+++ tools/development/helpers/clean.sh
@@ -4,7 +4,7 @@
 
 # @project        Library/IO
 # @file           tools/development/helpers/clean.sh
-# @author         Lucas Brémond <lucas@loftorbital.com>
+# @author         Lucas Brémond <lucas.bremond@gmail.com>
 # @license        Apache License 2.0
 
 ################################################################################################################################################################
