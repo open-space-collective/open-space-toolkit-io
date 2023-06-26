a01fc6235d11a5aad6442a66a70ee9707300fb62
Vishwa Shah <vishwa@loftorbital.com>
2023-06-26 18:28:19 +0000

    chore: clean up logic

--- tools/development/helpers/install-python.sh
+++ tools/development/helpers/install-python.sh
@@ -2,9 +2,7 @@
 
 # Apache License 2.0
 
-PYTHON_MAJOR_VERSION="3"
-PYTHON_MINOR_VERSION="11"
-PYTHON_VERSION="${PYTHON_MAJOR_VERSION}.${PYTHON_MINOR_VERSION}"
+PYTHON_VERSION="3.11"
 
 project_directory="$(git rev-parse --show-toplevel)"
 python_directory="${project_directory}/build/bindings/python/OpenSpaceToolkitIOPy-python-package-${PYTHON_VERSION}"
@@ -19,10 +17,10 @@ for link in ${links}
 
 do
 
-    dep=$(basename ${link})
+    dep=$(basename ${link} | tr '-' '_' | sed 's/\/$//')
 
-    package=$(echo "${dep}" | tr '-' '_' | sed 's/\/$//')
+    py_version=$(echo "${PYTHON_VERSION}" | sed 's/\.//')
 
-    python${PYTHON_VERSION} -m pip install /usr/local/share/${package}-*-py${PYTHON_MAJOR_VERSION}${PYTHON_MINOR_VERSION}-*.whl --force-reinstall;
+    python${PYTHON_VERSION} -m pip install /usr/local/share/${dep}-*-py${py_version}-*.whl --force-reinstall;
 
 done
