f246e55d39d2ca4071d7568acee437ef9bd4125d
Kyle Cochran <kyle.cochran@loftorbital.com>
2023-08-04 19:17:57 +0000

    style: formatting

--- bindings/python/test/ip/tcp/http/test_client.py
+++ bindings/python/test/ip/tcp/http/test_client.py
@@ -4,6 +4,7 @@ import pytest
 
 import ostk.io as io
 from ostk.core.filesystem import Directory, File, Path
+
 URL = io.URL
 Request = io.ip.tcp.http.Request
 Response = io.ip.tcp.http.Response
@@ -23,7 +24,7 @@ def test_http_client_get_success():
 
 def test_http_client_send_success():
     url = URL.parse("https://www.google.com")
-    
+
     request = Request(Request.Method.Get, url, "")
     response = Client.send(request)
 
@@ -34,7 +35,9 @@ def test_http_client_send_success():
 
 
 def test_http_client_fetch_success():
-    url = URL.parse("https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png")
+    url = URL.parse(
+        "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+    )
     directory = Directory.path(Path.parse("/tmp"))
 
     file_response = Client.fetch(url, directory)
@@ -45,13 +48,13 @@ def test_http_client_fetch_success():
 
 # def test_http_client_list_success():
 
-    # Disabling similar to the C++ test. The list takes about 1 minute to complete for some reason.
+# Disabling similar to the C++ test. The list takes about 1 minute to complete for some reason.
 
-    # url = URL.parse("ftp://naif.jpl.nasa.gov/pub/naif/generic_kernels/")
-    # file_input = File.path(Path.parse("/tmp/listing.txt")) 
-    # response = Client.list(url, file_input, 3)
+# url = URL.parse("ftp://naif.jpl.nasa.gov/pub/naif/generic_kernels/")
+# file_input = File.path(Path.parse("/tmp/listing.txt"))
+# response = Client.list(url, file_input, 3)
 
-    # assert response is not None
-    # assert response.is_defined() is True
-    # assert response.is_ok() is True
-    # assert response.get_status_code() == Response.StatusCode.Ok
+# assert response is not None
+# assert response.is_defined() is True
+# assert response.is_ok() is True
+# assert response.get_status_code() == Response.StatusCode.Ok
