c243db35681b30b18b14862ae4f0ed240cd5334c
Vishwa Shah <vishwa2710@gmail.com>
2024-02-13 09:00:30 +0000

    chore: remove __path__ from bindings

--- bindings/python/src/OpenSpaceToolkitIOPy/IP/TCP.cpp
+++ bindings/python/src/OpenSpaceToolkitIOPy/IP/TCP.cpp
@@ -7,8 +7,6 @@ inline void OpenSpaceToolkitIOPy_IP_TCP(pybind11::module& aModule)
     // Create "tcp" python submodule
     auto tcp = aModule.def_submodule("tcp");
 
-    // Add __path__ attribute for "tcp" submodule
-    tcp.attr("__path__") = "ostk.io.ip.tcp";
 
     // Add custom types to python "tcp" submodule
     OpenSpaceToolkitIOPy_IP_TCP_HTTP(tcp);
